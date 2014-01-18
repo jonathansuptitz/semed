2d5ed2abe5f9384c25e729aacd2b2e09bee21d1e
RobertoDebarba <roberto.debarba@gmail.com>
2014-01-18 17:07:24 -0200

    Atualização tema principla; Atualização gitignore;
    DB - Adicionada tabela tb_mural;

--- base/SEMEDcontratos.sql
+++ base/SEMEDcontratos.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `SEMEDcontratos` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `SEMEDcontratos`;
--- MySQL dump 10.13  Distrib 5.5.34, for debian-linux-gnu (x86_64)
+CREATE DATABASE  IF NOT EXISTS `semedcontratos` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `semedcontratos`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
 --
--- Host: 127.0.0.1    Database: SEMEDcontratos
+-- Host: localhost    Database: semedcontratos
 -- ------------------------------------------------------
--- Server version	5.5.34-0ubuntu0.13.10.1
+-- Server version	5.6.15
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -206,6 +206,31 @@ LOCK TABLES `tb_local_trabalho` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tb_mural`
+--
+
+DROP TABLE IF EXISTS `tb_mural`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_mural` (
+  `codigo_mural` int(11) NOT NULL,
+  `usuario_mural` varchar(45) NOT NULL,
+  `data_mural` varchar(15) NOT NULL,
+  `conteudo_mural` varchar(200) NOT NULL,
+  PRIMARY KEY (`codigo_mural`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_mural`
+--
+
+LOCK TABLES `tb_mural` WRITE;
+/*!40000 ALTER TABLE `tb_mural` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_mural` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tb_pessoas`
 --
 
@@ -269,4 +294,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-01-18 14:48:25
+-- Dump completed on 2014-01-18 16:59:35
--- source/umain.pas
+++ source/umain.pas
@@ -5,10 +5,22 @@ unit Umain;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
+  ExtCtrls, StdCtrls, Calendar, Menus;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Label1: TLabel;
+    MainMenu1: TMainMenu;
+    Memo1: TMemo;
+    MenuItem1: TMenuItem;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    StatusBar1: TStatusBar;
+    procedure MenuItem1Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -21,6 +33,13 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.MenuItem1Click(Sender: TObject);
+begin
+
+end;
 
 end.
 
