dfd62217effac0aba1e481842ea6dccf7d3ef048
RobertoDebarba <roberto.debarba@gmail.com>
2014-01-24 19:24:04 -0200

    Desenvolvimento tela Cargos

--- source/ucadastrocargos.pas
+++ source/ucadastrocargos.pas
@@ -25,20 +25,14 @@ type
     DBEdit2: TDBEdit;
     DBEdit3: TDBEdit;
     DBEdit4: TDBEdit;
-    DBEdit5: TDBEdit;
     DBGrid1: TDBGrid;
     dsCargos: TDatasource;
     editPesquisa: TEdit;
     GroupBox1: TGroupBox;
-    imgNovo: TImage;
-    imgEdit: TImage;
-    imgSalvar: TImage;
-    imgCancelar: TImage;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
-    Label5: TLabel;
     Label6: TLabel;
     Label7: TLabel;
     Label8: TLabel;
@@ -50,8 +44,12 @@ type
     procedure BtnNovoClick(Sender: TObject);
     procedure BtnSalvarClick(Sender: TObject);
     procedure BtnVoltarClick(Sender: TObject);
+    procedure ComboBox1Change(Sender: TObject);
+    procedure DBEdit1Change(Sender: TObject);
+    procedure editPesquisaChange(Sender: TObject);
     procedure editPesquisaKeyPress(Sender: TObject; var Key: char);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
   private
     { private declarations }
   public
@@ -60,14 +58,24 @@ type
 
 var
   frmCadastroCargos: TfrmCadastroCargos;
+  campoBusca, ScreenMode: string;
 
 implementation
+
+uses
+  dmMain;
 
 {$R *.lfm}
 
 { TfrmCadastroCargos }
 
 // INICIO ----------------------------------------------------------------------
+procedure TfrmCadastroCargos.FormCreate(Sender: TObject);
+begin
+  ScreenMode := 'mdNormal';
+  campoBusca := 'nome_cargo';
+end;
+
 procedure TfrmCadastroCargos.editPesquisaKeyPress(Sender: TObject; var Key: char
   );
 begin                                                          // Limpa campo de pesquisa
@@ -75,20 +83,72 @@ begin                                                          // Limpa campo de
     editPesquisa.Text := '';                                   // digitar
 end;
 
-// BOTOES ----------------------------------------------------------------------
-procedure TfrmCadastroCargos.BtnVoltarClick(Sender: TObject);       // Voltar
+// PESQUISA --------------------------------------------------------------------
+procedure TfrmCadastroCargos.editPesquisaChange(Sender: TObject);
 begin
-  self.Close;
+  if (campoBusca = 'codigo_cargo') then               // Se opção selecionada for código
+  begin
+    if (Length(editPesquisa.Text) = 0) then             // Se usuario apagar todos os caracteres do campo
+      DM1.tb_cargos.Filtered := false                     // Desabilita filtro para evitar erro
+    else
+    begin                                                 // Senao executa filtro normalmente
+      DM1.tb_cargos.Filter := campoBusca+' = '+ editPesquisa.Text;
+      DM1.tb_cargos.Filtered := true; {Pesquisa para código apenas com "="}
+    end;
+  end
+  else
+  begin                                              // Se opção selecionada for código -> executa filtro normalmente
+    DM1.tb_cargos.Filter := campoBusca+' LIKE '+ QuotedStr('*' + editPesquisa.Text + '*');
+    DM1.tb_cargos.Filtered := true;
+  end;
+end;
+
+procedure TfrmCadastroCargos.ComboBox1Change(Sender: TObject);
+begin
+  if (ComboBox1.Text = 'Código') then                // Altera var com o valor do
+    campoBusca := 'codigo_cargo'                     // campo a ser pesquisado quando
+  else if (ComboBox1.Text = 'Descrição') then        // usuario muda comboBox
+    campoBusca := 'nome_cargo'
+  else if (ComboBox1.Text = 'Grupo Ocupacional') then
+    campoBusca := 'grupo_ocupacional_cargo';
+end;
+
+// CONTROLE DE CLAUSULA - MEMO -------------------------------------------------
+procedure TfrmCadastroCargos.DBEdit1Change(Sender: TObject); // Quando muda registro,
+begin                                                        // carrega Clau. prim. para
+  memoClau.Lines.Clear;                                      // memo
+  if (FileExists('clp/clp'+DBEdit1.Text+'.txt')) then
+    if (ScreenMode <> 'mdInsert') then               // Se INSERT nao estiver ativo
+      memoClau.Lines.LoadFromFile('clp/clp'+DBEdit1.Text+'.txt')
+  else
+    ShowMessage('Ocorreu um erro ao carregar Cláusula Primeira!');
+end;
+
+// BOTOES ----------------------------------------------------------------------
+procedure TfrmCadastroCargos.BtnSalvarClick(Sender: TObject);       // Salvar
+var
+  SLtrans: TStringList;
+begin
+  dsCargos.DataSet.Post;  // Salvar registro
+  SLtrans := TStringList.Create;
+  SLtrans.Clear;
+
+  //memoClau.Lines.save(Sltrans); // Salva memo com base no codigo do registro
+  memoClau.Lines.SaveToFile('clp/clp'+DBEdit1.Text+'.txt');
+  SLtrans.Free;
+  ScreenMode := 'mdNormal';
+  //memoClau.Lines.LoadFromFile('clp/clp'+DBEdit1.Text+'.txt')
 end;
 
 procedure TfrmCadastroCargos.BtnNovoClick(Sender: TObject);         // Novo
 begin
+  ScreenMode := 'mdInsert';  // Marcação para não carregar quando o registro é novo
   dsCargos.DataSet.Insert;
 end;
 
-procedure TfrmCadastroCargos.BtnSalvarClick(Sender: TObject);       // Salvar
+procedure TfrmCadastroCargos.BtnVoltarClick(Sender: TObject);       // Voltar
 begin
-  dsCargos.DataSet.Post;
+  self.Close;
 end;
 
 procedure TfrmCadastroCargos.BtnEditarClick(Sender: TObject);       // Editar
@@ -103,6 +163,7 @@ end;
 
 procedure TfrmCadastroCargos.BtnCancelarClick(Sender: TObject);     // Cancelar
 begin
+  ScreenMode := 'mdNormal';
   dsCargos.DataSet.Cancel;
 end;
 
