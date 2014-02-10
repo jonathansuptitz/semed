4b207315cdd61a31623438b05f588dc829a3dbac
RobertoDebarba <roberto.debarba@gmail.com>
2014-02-09 22:17:38 -0200

    Previsão de erros tela de cargos (tela concluida)

--- source/ucadastrocargos.pas
+++ source/ucadastrocargos.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, db, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  Buttons, StdCtrls, DbCtrls, DBGrids;
+  Buttons, StdCtrls, DbCtrls, DBGrids, LCLType;
 
 type
 
@@ -46,10 +46,14 @@ type
     procedure BtnVoltarClick(Sender: TObject);
     procedure ComboBox1Change(Sender: TObject);
     procedure DBEdit1Change(Sender: TObject);
+    procedure DBEdit4KeyPress(Sender: TObject; var Key: char);
+    procedure DBGrid1Enter(Sender: TObject);
     procedure editPesquisaChange(Sender: TObject);
     procedure editPesquisaKeyPress(Sender: TObject; var Key: char);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
+    procedure EditON;
+    procedure EditOFF;
   private
     { private declarations }
   public
@@ -59,6 +63,7 @@ type
 var
   frmCadastroCargos: TfrmCadastroCargos;
   campoBusca, ScreenMode: string;
+  SelecionarAtivo: boolean;
 
 implementation
 
@@ -74,6 +79,8 @@ procedure TfrmCadastroCargos.FormCreate(Sender: TObject);
 begin
   ScreenMode := 'mdNormal';
   campoBusca := 'nome_cargo';
+  EditOFF;
+  SelecionarAtivo := false;
 end;
 
 procedure TfrmCadastroCargos.editPesquisaKeyPress(Sender: TObject; var Key: char
@@ -81,6 +88,54 @@ procedure TfrmCadastroCargos.editPesquisaKeyPress(Sender: TObject; var Key: char
 begin                                                          // Limpa campo de pesquisa
   if (editPesquisa.Text = 'Digite para pesquisar...') then     // quando usuario começa a
     editPesquisa.Text := '';                                   // digitar
+end;
+
+// PREVISAO de ERROS -----------------------------------------------------------
+
+procedure TfrmCadastroCargos.DBEdit4KeyPress(Sender: TObject; var Key: char); //Masc Moeda
+begin
+  if not (Key in ['0'..'9', '.', #8{backspace}]) then
+    Key := #0{nil};
+end;
+
+procedure TfrmCadastroCargos.DBGrid1Enter(Sender: TObject); // Ao selecionar GRID
+begin
+  EditOFF;
+end;
+
+procedure TfrmCadastroCargos.EditON;        // Habilitar Edição
+begin
+  DBEdit2.ReadOnly := false;
+  DBEdit3.ReadOnly := false;
+  DBEdit4.ReadOnly := false;
+  memoClau.ReadOnly := false;
+
+  BtnSalvar.Enabled := true;
+  BtnCancelar.Enabled := true;
+  BtnNovo.Enabled := false;
+  BtnEditar.Enabled := false;
+  BtnApagar.Enabled := false;
+  BtnSelecionar.Enabled := false;
+  BtnVoltar.Enabled := false;
+  if BtnSelecionar.Enabled then
+    SelecionarAtivo := true; // Marca estado inicial do BtnSelecionar
+end;
+
+procedure TfrmCadastroCargos.EditOFF;       // Desabilitar Edição
+begin
+  DBEdit2.ReadOnly := true;
+  DBEdit3.ReadOnly := true;
+  DBEdit4.ReadOnly := true;
+  memoClau.ReadOnly := true;
+
+  BtnSalvar.Enabled := false;
+  BtnCancelar.Enabled := false;
+  BtnNovo.Enabled := true;
+  BtnEditar.Enabled := true;
+  BtnApagar.Enabled := true;
+  BtnVoltar.Enabled := true;
+  if SelecionarAtivo then  // Verifica se BtnSelecionar esava ativo inicialmente
+    BtnSelecionar.Enabled := true;
 end;
 
 // PESQUISA --------------------------------------------------------------------
@@ -101,6 +156,8 @@ begin
     DM1.tb_cargos.Filter := campoBusca+' LIKE '+ QuotedStr('*' + editPesquisa.Text + '*');
     DM1.tb_cargos.Filtered := true;
   end;
+
+  EditOFF;
 end;
 
 procedure TfrmCadastroCargos.ComboBox1Change(Sender: TObject);
@@ -111,6 +168,8 @@ begin
     campoBusca := 'nome_cargo'
   else if (ComboBox1.Text = 'Grupo Ocupacional') then
     campoBusca := 'grupo_ocupacional_cargo';
+
+  EditOFF;
 end;
 
 // CONTROLE DE CLAUSULA - MEMO -------------------------------------------------
@@ -129,28 +188,39 @@ procedure TfrmCadastroCargos.BtnSalvarClick(Sender: TObject);       // Salvar
 var
   SLtrans: TStringList;
 begin
-  //salva o caminho da clausula no banco
-  dsCargos.DataSet.FieldByName('clausula_primeira_cargo').value := 'clp/clp'+DBEdit1.Text+'.txt';
+  if (DBEdit2.Text = '') or (DBEdit3.Text = '') or (DBEdit4.Text = '') or (memoClau.Text = '') then
+    ShowMessage('Todos os campos são obrigatorios!')
+  else
+  begin
+    //salva o caminho da clausula no banco
+    dsCargos.DataSet.FieldByName('clausula_primeira_cargo').value := 'clp/clp'+DBEdit1.Text+'.txt';
 
-  dsCargos.DataSet.Post;  // Salvar registro
+    dsCargos.DataSet.Post;  // Salvar registro
 
-  try
-    SLtrans := TStringList.Create;
+    try
+      SLtrans := TStringList.Create;
 
-    SLtrans.Assign(memoClau.Lines);//atribui memo na stringlist
+      SLtrans.Assign(memoClau.Lines);//atribui memo na stringlist
 
-    SLtrans.SaveToFile('clp/clp'+DBEdit1.Text+'.txt');  //salva a stringlist direto no arquivo
-  finally
-    SLtrans.Free;//libera stringlist da memoria
+      SLtrans.SaveToFile('clp/clp'+DBEdit1.Text+'.txt');  //salva a stringlist direto no arquivo
+    finally
+      SLtrans.Free;//libera stringlist da memoria
+    end;
+
+    ShowMessage('Registro salvo com sucesso!');
+
+    ScreenMode := 'mdNormal';
+
+    EditOFF;
   end;
-
-  ScreenMode := 'mdNormal';
 end;
 
 procedure TfrmCadastroCargos.BtnNovoClick(Sender: TObject);         // Novo
 begin
   ScreenMode := 'mdInsert';  // Marcação para não carregar quando o registro é novo
   dsCargos.DataSet.Insert;
+
+  EditON;
 end;
 
 procedure TfrmCadastroCargos.BtnVoltarClick(Sender: TObject);       // Voltar
@@ -161,17 +231,26 @@ end;
 procedure TfrmCadastroCargos.BtnEditarClick(Sender: TObject);       // Editar
 begin
   dsCargos.DataSet.Edit;
+
+  EditON;
 end;
 
 procedure TfrmCadastroCargos.BtnApagarClick(Sender: TObject);       // Apagar
 begin
-  dsCargos.DataSet.Delete;
+  if Application.MessageBox('Deseja realmente apagar o registro?','Apagar registro', MB_YESNO) = idYES then
+  begin
+    dsCargos.DataSet.Delete;
+    ShowMessage('Registro apagado com sucesso!');
+    EditOFF;
+  end;
 end;
 
 procedure TfrmCadastroCargos.BtnCancelarClick(Sender: TObject);     // Cancelar
 begin
   ScreenMode := 'mdNormal';
   dsCargos.DataSet.Cancel;
+
+  EditOFF;
 end;
 
 // FIM -------------------------------------------------------------------------
